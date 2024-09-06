cb3392def676bde3f286fa704b437868738faa7e
Narges Jahromi <nrgs.jahromi@gmail.com>
2024-09-06 20:21:48 +0330

    redesign paymentConfirmation

--- src/components/cart/PaymentConfirmation.tsx
+++ src/components/cart/PaymentConfirmation.tsx
@@ -12,16 +12,15 @@ import {
   CircularProgress,
   Divider,
 } from "@mui/material";
-import { QRCodeCanvas } from "qrcode.react"; 
+import Barcode from "react-barcode"; // کتابخانه برای تولید بارکد
 import { lighten } from "@mui/material/styles";
 import { useInvoiceDetail } from "../../api/invoice/getInvoiceDetail";
 import theme from "../../theme";
 import { useParams } from "react-router-dom";
 import { TickCircle } from "iconsax-react";
 
-const PaymentConfirmation = () => {
+const PaymentConfirmation: FC = () => {
   const { invoiceId } = useParams<{ invoiceId: string }>();
-  const qrRef = useRef<HTMLCanvasElement | null>(null);
 
   const {
     data: invoiceDetail,
@@ -41,51 +40,39 @@ const PaymentConfirmation = () => {
 
   return (
     <Box
+    height={"100%"}
+      paddingBottom={10}
+      className="flex flex-col justify-between "
+      
       sx={{
-        maxWidth: 800,
-        margin: "auto",
         padding: 4,
-        // bgcolor: "background.paper",
-        borderRadius: 2,
-        // boxShadow: 3,
       }}
     >
-      <Box
-        className="w-full items-center flex justify-center"
-        sx={{ color: theme.palette.success.light }}
-      >
-        <TickCircle variant="Bold" size={90} />
-      </Box>
-
-      <Box mb={2} className="w-full">
-        <Typography variant="body1" fontWeight="bold" align="center">
-          خرید شما از {invoiceDetail?.store} با موفقیت انجام شد.
-        </Typography>
-      </Box>
-      <Box className=" my-4 flex flex-col justify-end items-center">
-        <QRCodeCanvas
-          ref={qrRef}
-          value={invoiceId!}
-          title={"Title for my QR Code"}
-          size={180}
-          bgColor={theme.palette.background.default}
-          fgColor={theme.palette.text.primary}
-          level={"Q"}
-          marginSize={0}
-          // imageSettings={{
-          //   src: logo,
-          //   x: undefined,
-          //   y: undefined,
-          //   height: 45,
-          //   width: 45,
-          //   opacity: 1,
-          //   excavate: true,
-          // }}
-        /></Box>
-      <TableContainer component={Paper}>
+      <Box height={"fit-content"}>
+        <Box
+          className="w-full items-center flex flex-col justify-center"
+          sx={{ color: theme.palette.success.light }}
+        >
+          <TickCircle variant="Bold" size={90} />
+         
+        </Box> <Typography variant="body1" fontWeight="bold" align="center">
+            خرید شما از {invoiceDetail?.store} با موفقیت انجام شد.
+          </Typography>
+        <Box className=" my-4 flex flex-col justify-end items-center">
+          <Barcode
+            value={invoiceId!}
+            width={2}
+            height={50}
+            format="CODE128" 
+            displayValue={false}
+            background={theme.palette.background.default}
+            lineColor={theme.palette.text.primary}
+          />
+        </Box>
+        <TableContainer component={Paper} sx={{maxHeight:"45vh" , overflow:"auto"}}>
         <Table size="small">
-          <TableHead>
-            <TableRow>
+          <TableHead sx={{position:"sticky" , top:0}}>
+            <TableRow >
               <TableCell
                 sx={{
                   backgroundColor: lighten(theme.palette.primary.main, 0.2),
@@ -150,40 +137,45 @@ const PaymentConfirmation = () => {
         </Table>
       </TableContainer>
 
-      <Box className="w-full flex justify-between items-center mt-6">
-        <Typography variant="body2">تعداد کالا</Typography>
-        <Typography variant="body1" fontWeight={"bold"}>
-          {totalItems}
-        </Typography>
       </Box>
-      <Box className="w-full flex justify-between items-center">
-        <Typography variant="body2">تخفیف</Typography>
-        <Typography variant="body1" fontWeight={"bold"}>
-          {discount.toLocaleString()}
-        </Typography>
-      </Box>
-      <Box className="w-full flex justify-between items-center">
-        <Typography variant="body2">مالیات</Typography>
-        <Typography variant="body1" fontWeight={"bold"}>
-          {invoiceDetail.tax.toLocaleString()}
-        </Typography>
-      </Box>
-      <Box className="w-full flex justify-between items-center">
-        <Typography variant="body2">جمع کل (بدون تخفیف)</Typography>
-        <Typography variant="body1" fontWeight={"bold"}>
-          {totalAmount.toLocaleString()}
-        </Typography>
-      </Box>
-      <Divider sx={{ my: 2 }} />
-      <Box className="w-full flex justify-between items-center">
-        <Typography variant="body1">مجموع قابل پرداخت</Typography>
-        <Typography
-          variant="body1"
-          fontWeight={"bold"}
-          color={theme.palette.primary.main}
-        >
-          {invoiceDetail.payable_amount.toLocaleString()} تومان
-        </Typography>
+
+      
+      <Box>
+        <Box className="w-full flex justify-between items-center mt-6">
+          <Typography variant="body2">تعداد کالا</Typography>
+          <Typography variant="body1" fontWeight={"bold"}>
+            {totalItems}
+          </Typography>
+        </Box>
+        <Box className="w-full flex justify-between items-center">
+          <Typography variant="body2">تخفیف</Typography>
+          <Typography variant="body1" fontWeight={"bold"}>
+            {discount.toLocaleString()}
+          </Typography>
+        </Box>
+        <Box className="w-full flex justify-between items-center">
+          <Typography variant="body2">مالیات</Typography>
+          <Typography variant="body1" fontWeight={"bold"}>
+            {invoiceDetail.tax.toLocaleString()}
+          </Typography>
+        </Box>
+        <Box className="w-full flex justify-between items-center">
+          <Typography variant="body2">جمع کل (بدون تخفیف)</Typography>
+          <Typography variant="body1" fontWeight={"bold"}>
+            {totalAmount.toLocaleString()}
+          </Typography>
+        </Box>
+        <Divider sx={{ my: 2 }} />
+        <Box className="w-full flex justify-between items-center">
+          <Typography variant="body1">مجموع قابل پرداخت</Typography>
+          <Typography
+            variant="body1"
+            fontWeight={"bold"}
+            color={theme.palette.primary.main}
+          >
+            {invoiceDetail.payable_amount.toLocaleString()} تومان
+          </Typography>
+        </Box>
       </Box>
     </Box>
   );
